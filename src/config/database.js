88605b5d3112d1c802f8f0195bb31f019b82d463
Alison Leme <alisonletos@hotmail.com>
2022-07-14 00:28:07 -0300

    Iniciando configurações do servidor com express - configurações iniciais do banco de dados Postgres SQL com Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "ruby.db.elephantsql.com",
+  username: "nftzldbe",
+  password: "f9tKD0q_6kZ82TLP5KSR4w9Id9pTnNPC",
+  database: "nftzldbe",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
